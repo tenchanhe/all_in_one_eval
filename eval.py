b619541fedefb173f20879b1dfc4634def074021
tenchanhe <chanhe0719@gmail.com>
2024-11-05 16:51:30 +0800

    backup: 1105

--- eval.py
+++ eval.py
@@ -19,7 +19,19 @@ def run_llm(prompt, model):
     return response
 
 
-def eval(questions, A, B, C, D, model, lang):
+def get_response(questions, A, B, C, D, model, lang):
     prompt = get_prompt(questions, A, B, C, D, lang)
     response = run_llm(prompt, model)
     return response
+
+
+def get_score(result, answer):
+    hit = 0
+    if len(result) != len(answer):
+        raise ValueError('length error')
+    else:
+        for i in range(len(result)):
+            if result[i] == answer[i]:
+                hit += 1
+
+    return hit/len(result)
