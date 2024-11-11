b3056be927564c62ddaabf7d08ab917566f0cb76
tenchanhe <chanhe0719@gmail.com>
2024-11-11 11:53:22 +0800

    backup 1111

--- par_to_csv.py
+++ par_to_csv.py
@@ -7,21 +7,17 @@ def parquet_to_csv_folder(folder_path):
         if file_name.endswith(".parquet"):
             parquet_file_path = os.path.join(folder_path, file_name)
             csv_file_name = file_name.replace(".parquet", ".csv")
-            csv_file_path = os.path.join(folder_path, csv_file_name)
+            csv_file_path = os.path.join("./data/mmlu_csv" , csv_file_name)
             
             df = pd.read_parquet(parquet_file_path)
-             
-            for column in df.columns:
-                if df[column].dtype == 'object' and isinstance(df[column].iloc[0], list):
-                    df[column] = df[column].apply(lambda x: ','.join(map(str, x)) if isinstance(x, list) else x)
 
-            df.to_csv(csv_file_path, index=False)
+            df.to_csv(csv_file_path, index=True)
             print(f"File converted: {parquet_file_path} to {csv_file_path}")
 
 
-# folder_path = "./data/mmlu_parquet"  
-# parquet_to_csv_folder(folder_path)
+folder_path = "./data/mmlu_parquet"  
+parquet_to_csv_folder(folder_path)
 
-df = pd.read_parquet('./data/mmlu_parquet/abstract_algebra.parquet')
-print(type(df["choices"]))
-print(df["choices"])
+# df = pd.read_parquet('./data/mmlu_parquet/abstract_algebra.parquet')
+# print(type(df["choices"]))
+# print(df["choices"])
